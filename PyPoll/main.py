c45e9904990e3d93c14c2752750778f5d2be4c16
Laurel Williamson <laurelwilliamson@Laurels-MacBook-Pro.local>
2020-11-28 23:42:27 -0500

    commit all but txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,8 +11,31 @@ total_votes = len(election_df.index)
 print(f"Total Votes: " + str(total_votes))
 print("-------------------------")
 #A complete list of candidates who received votes
-Candidates = election_df["Candidate"].unique()
-print(Candidates)
+
+candidates_names = election_df["Candidate"].unique()
+candidates_count = election_df["Candidate"].value_counts()
+candidates_percent = election_df["Candidate"].value_counts(normalize=True).mul(100).round(3).astype(str)+ '%' 
+
+max = 0
+for i in range(0,len(candidates_percent)):
+    if max < candidates_count[i]:
+        max = candidates_count[i]
+        winner = candidates_names[i]
+    print(str(candidates_names[i])+':', candidates_percent[i], '('+ str(candidates_count[i])+ ')')
+print("-------------------------")
+print("Winner:", winner)
+print("-------------------------")
+
+
+
+# widths = [max(map(len, col)) for col in zip(*rows)]
+# for row in rows:
+#     print "  ".join((val.ljust(width) for val, width in zip(row, widths)))
+
+#indx = index.election_df["Candidate"].value_counts()
+#print(indx)
+
+
 
 #The percentage of votes each candidate won
 
