9dee305b7d1e108f6d08e298c9c8504acb4cd6b4
Laurel Williamson <laurelwilliamson@Laurels-MacBook-Pro.local>
2020-11-28 21:17:10 -0500

    first commit

---
+++ PyPoll/main.py
@@ -0,0 +1,20 @@
+#Import modules
+import pandas as pd
+#Create reference to CSV file
+csvpath = "Resources/election_data.csv"
+#Import the CSV into a pandas DataFrame
+election_df = pd.read_csv(csvpath, low_memory=False)
+#The total number of votes cast
+total_votes = len(election_df.index)
+print(total_votes)
+#A complete list of candidates who received votes
+Candidates = election_df["Candidate"].unique()
+print(Candidates)
+
+#The percentage of votes each candidate won
+
+
+#The total number of votes each candidate won
+
+
+#The winner of the election based on popular vote.
