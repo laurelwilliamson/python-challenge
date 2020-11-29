50559813ecdd9a558b8343f73af848fd4f0a7e67
Laurel Williamson <laurelwilliamson@Laurels-MacBook-Pro.local>
2020-11-28 22:36:02 -0500

    everything works, just need text import

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,13 +4,12 @@ import pandas as pd
 csvpath = "Resources/budget_data.csv"
 #Import the CSV into a pandas DataFrame
 budget_df = pd.read_csv(csvpath, low_memory=False)
-budget_df
 #Calculate the total number of months included in the dataset
 total_months = len(budget_df.index)
-print(f"There are " + str(total_months) + " total months in the dataset.")
+print(f"Total Months:  " + str(total_months))
 #Calculate the net total amount of "Profit"/Losses" over the entire period'
 Total = budget_df["Profit/Losses"].sum()
-print(Total)
+print(f"Total: $" + str(Total))
 #Calculate the changes in "Profits/Losses" over the entire period and find their average
 Profit_loss = budget_df["Profit/Losses"]
 
@@ -32,8 +31,25 @@ Changes = budget_deltas[0]
 Sum_of_Changes = budget_deltas[1]
 Average_Change = budget_deltas[2]
 
+
+print(f"Average Change: $" + str(round(Average_Change, 2)))
 Greatest_Profit = max(Changes)
-print(Greatest_Profit)
+Greatest_Loss = min(Changes)
+
+indexChanges_Loss = Changes.index(Greatest_Loss)+1
+indexChanges_Profit = Changes.index(Greatest_Profit)+1
+
+date_loss = budget_df["Date"][indexChanges_Loss]
+date_profit = budget_df["Date"][indexChanges_Profit]
+
+print(f"Greatest Increase in Price: " + str(date_profit) + " ($" + str(Greatest_Profit) + ")")
+print(f"Greatest Decrease in Price: " + str(date_loss) + " ($" + str(Greatest_Loss) + ")")
+# new_ind = budget_df.set_index("Profit/Losses")
+
+# ind_df = budget_df.loc[budget_df["Profit/Losses"]==Greatest_Loss]
+# print(ind_df)
+
+
 
 
 #Find greatest increase in profits (date&amount) over entire period
